6c82949a65945e7f19dcace6a1d0ca6758f8c09d
jk0bb <80794686+jk0bb@users.noreply.github.com>
2025-05-14 10:11:47 -0400

    Create archives_and_exports_in_courses.sql

---
+++ archives_and_exports_in_courses.sql
@@ -0,0 +1,33 @@
+
+SELECT SPLIT_PART(xu.full_path, '/', 4) AS course_id, xu.full_path, xf.creation_date, xf.file_size
+FROM xyf_urls xu, xyf_files xf
+WHERE xu.file_name LIKE '%ArchiveFile%'
+AND xf.file_id = xu.file_id
+AND xu.file_name LIKE '%.zip'
+AND xu.full_path LIKE '/internal%'
+AND xf.creation_date < DATE_TRUNC('day', NOW() - INTERVAL '1 month')
+OR xu.file_name LIKE 'ExportFile%'
+AND xf.file_id = xu.file_id
+AND xu.file_name LIKE '%.zip'
+AND xu.full_path LIKE '/internal%'
+AND xf.creation_date < DATE_TRUNC('day', NOW() - INTERVAL '1 month')
+OR xu.file_name LIKE 'CommonCartridge%'
+AND xf.file_id = xu.file_id
+AND xu.file_name LIKE '%.zip'
+AND xu.full_path LIKE '/internal%'
+AND xf.creation_date < DATE_TRUNC('day', NOW() - INTERVAL '1 month')
+OR xu.file_name LIKE '%ArchiveFile%'
+AND xf.file_id = xu.file_id
+AND xu.file_name LIKE '%.imscc'
+AND xu.full_path LIKE '/internal%'
+AND xf.creation_date < DATE_TRUNC('day', NOW() - INTERVAL '1 month')
+OR xu.file_name LIKE 'ExportFile%'
+AND xf.file_id = xu.file_id
+AND xu.file_name LIKE '%.imscc'
+AND xu.full_path LIKE '/internal%'
+AND xf.creation_date < DATE_TRUNC('day', NOW() - INTERVAL '1 month')
+OR xu.file_name LIKE 'CommonCartridge%'
+AND xf.file_id = xu.file_id
+AND xu.file_name LIKE '%.imscc'
+AND xu.full_path LIKE '/internal%'
+AND xf.creation_date < DATE_TRUNC('day', NOW() - INTERVAL '1 month')
