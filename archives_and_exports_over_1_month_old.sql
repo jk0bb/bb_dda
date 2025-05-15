774587bd623198782cb39abd0fc46a1511e5f36e
jk0bb <80794686+jk0bb@users.noreply.github.com>
2025-05-15 09:25:23 -0400

    Update and rename archives_and_exports_in_courses.sql to archives_and_exports_over_1_month_old.sql

--- archives_and_exports_over_1_month_old.sql
+++ archives_and_exports_over_1_month_old.sql
@@ -1,4 +1,5 @@
 -- Although Blackboard claims that archives and exports are automatically deleted after 30 days, that's not always the case.
+-- This will find all archives and exports that are over 30 days old.
 SELECT SPLIT_PART(xu.full_path, '/', 4) AS course_id, xu.full_path, xf.creation_date, xf.file_size
 FROM xyf_urls xu, xyf_files xf
 WHERE xu.file_name LIKE '%ArchiveFile%'
